e309b6a3391506a532bddadc9e566771e298686c
Will <williscodingnow@gmail.com>
2017-03-26 14:48:53 -0700

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -10,16 +10,33 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
+        # Billy heard about an awesome new online to-do app that's super handy.
+        # He goes to check it out.
         self.browser.get('http://localhost:8000')
 
-        # She notices the page title and header mention to-do lists
+        # Billy notices the page title and the header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
-        # She is invited to enter a to-do item straight away
-        [...rest of comments as before]
+        # He is invited to enter a to-do item straight away
+
+        # He types "Clean out the car" into the text box
+        # ( He's been dreading it for days )
+
+        # When he hits enter, the page updates, and now the page lists
+        # "1: Clean out the car" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. He enters
+        # "Clean my computer" ( as it desperately needs a cleaning )
+
+        # The page updates again, and now shows both items on his list
+
+        # Billy wonders whether the site will remember his list. Then he sees
+        # that the site has generated a unique URL for her -- there is text explaining it
+
+        # He visits that URL - and huzzah his list is still there
+
+        # Now satisfied, he returns to finishing lunc
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
